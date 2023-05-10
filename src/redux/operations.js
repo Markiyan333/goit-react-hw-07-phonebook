159f442885728c589cc6ac0823d71271854e38a4
Markiyan333 <markiyan333@gmail.com>
2023-05-10 22:14:45 +0300

    update operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+
+
 axios.defaults.baseURL = 'https://643c339d6afd66da6ada02dd.mockapi.io/';
 
 const fetchContacts = createAsyncThunk(
